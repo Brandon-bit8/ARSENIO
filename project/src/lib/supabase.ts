d73cc23d7eac2c485657db31f2177d3de50217ab
Brandon-bit8 <87790160+Brandon-bit8@users.noreply.github.com>
2024-11-21 14:33:48 -0600

    Delete project directory

--- project/src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://ydjnahhzdhmbanqipmop.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
